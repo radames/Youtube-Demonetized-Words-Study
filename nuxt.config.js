3e1e861ea9ff19360467ed8df695cd31363183ab
Radames Ajna <radamajna@gmail.com>
2019-12-05 12:53:54 -0800

    replace default favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,27 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: 'favicon/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: 'favicon/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: 'favicon/favicon-16x16.png'
+      },
+      { rel: 'manifest', href: 'favicon/site.webmanifest' }
+    ]
   },
   /*
    ** Customize the progress-bar color
