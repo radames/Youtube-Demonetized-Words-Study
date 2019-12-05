a6976444adb39da6034db169c78c65d0a76bc2e4
Radames <radames@manga.lan>
2019-12-05 01:26:57 -0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div ref="chart"></div>
+  </div>
+</template>
+
+<script>
+import { Runtime, Inspector } from '@observablehq/runtime'
+import notebook from '@radames/youtube-demonetized-words-similarity-study'
+
+export default {
+  components: {},
+  data() {
+    return {
+      notebookModule: null
+    }
+  },
+  mounted() {
+    const runtime = new Runtime()
+    this.notebookModule = runtime.module(notebook, (name) => {
+      if (name === 'chart') {
+        return new Inspector(this.$refs.chart)
+      }
+    })
+  }
+}
+</script>
+
+<style>
+/* .container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+} */
+</style>
